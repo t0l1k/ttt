ca273a6766617f49db375dcbdd4c5b47144103d5
Anatoli Tjushka <t0l1k@mail.ru>
2019-08-07 09:21:28 +0300

    init

---
+++ app/src/main/java/t0l1k/ttt/MainActivity.java
@@ -0,0 +1,14 @@
+package t0l1k.ttt;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
