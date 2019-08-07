43191682e84df2ca58c0471aeaecf49dc0bb056b
Anatoli Tjushka <t0l1k@mail.ru>
2019-08-07 10:07:45 +0300

    добавил обработчик событий кнопок

--- app/src/main/java/t0l1k/ttt/MainActivity.java
+++ app/src/main/java/t0l1k/ttt/MainActivity.java
@@ -3,6 +3,8 @@ package t0l1k.ttt;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    protected void gameLogic(View view){
+        ((Button)view).setText("X");
     }
 }
